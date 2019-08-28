0cbe3ba91f21e5be53edc3fe9058964e9e6eb4ca
Andrey <drunya35@gmail.com>
2019-08-28 19:40:49 +0300

    add yandex music

--- src/Components/Music/Music.js
+++ src/Components/Music/Music.js
@@ -5,10 +5,18 @@ const Music = () => {
 
     return (
         <div>
-            Music
-
+            <iframe frameBorder="1"
+                styles="border:none;width:100%;height:100%;"
+                width="250%"
+                height="300%"
+                src="https://music.yandex.ru/iframe/#playlist/yamusic-trending/1000/show/cover/description/">Слушайте
+            <a href='https://music.yandex.ru/users/yamusic-trending/playlists/1000'>Хайп
+            </a> —
+            <a href='https://music.yandex.ru/users/yamusic-trending'>Яндекс.Музыка
+            </a> на Яндекс.Музыке
+            </iframe>
         </div>
-    );
+    )
 }
 
 export default Music
